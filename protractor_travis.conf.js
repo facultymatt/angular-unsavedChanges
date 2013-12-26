47986d1895f9e71c11fb28d3da153204b20ab99c
Matt Miller <matt@facultycreative.com>
2013-12-26 14:02:25 -0500

    working on protractor config for travis.

--- protractor_travis.conf.js
+++ protractor_travis.conf.js
@@ -10,10 +10,6 @@ exports.config = {
   //    running.
   // 3. sauceUser/sauceKey - to use remote Selenium servers via SauceLabs.
 
-  // The location of the selenium standalone server .jar file.
-  //seleniumServerJar: './selenium/selenium-server-standalone-2.35.0.jar',
-
-  seleniumAddress: 'http://127.0.0.1:4444/wd/hub',
   // The port to start the selenium server on, or null if the server should
   // find its own unused port.
   seleniumPort: null,
@@ -26,6 +22,11 @@ exports.config = {
   // if you need to change the browser timeout, use
   // seleniumArgs: ['-browserTimeout=60'],
   seleniumArgs: [],
+
+  // If sauceUser and sauceKey are specified, seleniumServerJar will be ignored.
+  // The tests will be run remotely using SauceLabs.
+  sauceUser: 'facultymatt',
+  sauceKey: 'b280b942-1965-446d-90bf-e069b5cd2cf9',
 
   // ----- What tests to run -----
   //
