6ba2e76bac5382e52708378bba3a755d3f8242dd
Matt Miller <matt@facultycreative.com>
2013-12-27 08:33:53 -0500

    changes.

--- karma-unit.conf.js
+++ karma-unit.conf.js
@@ -59,7 +59,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
